49d4c9147f880aa7c6a9469fe0b8e7da5f03ccbb
Mizanur Rahman <newmizanur@gmail.com>
2022-05-17 02:46:29 +0800

    updated deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   deploy : {
     odoo : {
-      user : 'ec2-user',
+      user : 'ubuntu',
       host : 'erp.rpdgroup.deploy',
       ref  : `prod_15`,
       repo : 'git@github.com:newmizanur/odoo.git',
-      path : '/home/ec2-user/webapps/odoo',
+      path : '/home/ubuntu/webapps/odoo',
       ssh_options: "StrictHostKeyChecking=no",
-      fetch: 'full',
+      fetch: 'fast',
       'post-deploy' : [
+          'sudo systemctl restart odoo.service'
       ].join(' && ')
     },
   }
@@ -19,3 +20,10 @@ module.exports = {
 * Example commands:
 * pm2 deploy odoo
 * */
+
+//Installed by following articles, 
+// https://www.cybrosys.com/blog/how-to-install-odoo-15-on-ubuntu-2004-lts-server
+// https://speedysense.com/install-odoo-15-on-ubuntu-20-04/
+
+// For Error: pg_config executable not found, run -> sudo apt-get install libpq-dev
+// To install wkhtmltopdf -> sudo apt-get install wkhtmltopdf
